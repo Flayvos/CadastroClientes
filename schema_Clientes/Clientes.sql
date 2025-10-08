e78c8063dabca9bd8df6b4bf0d34642294bec0f1
flayvos777 <flayvos777@gmail.com>
2025-10-08 15:48:17 +0900

    Segundo Commit

---
+++ schema_Clientes/Clientes.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE cadastro_clientes;
+USE cadastro_clientes;
+
+CREATE TABLE cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    cpf VARCHAR(20),
+    email VARCHAR(100),
+    telefone VARCHAR(20)
+);
